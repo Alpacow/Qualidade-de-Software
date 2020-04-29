3cf206060c9f0dc26c9d1e5ef4b71f674d92e136
Alpacow <fvpereira@inf.ufsm.br>
2020-04-28 22:43:32 -0300

    Iniciado repo junit com maven

---
+++ T1/mavenJunit/src/main/java/com/t1/mavenJunit/App.java
@@ -0,0 +1,13 @@
+package com.t1.mavenJunit;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
