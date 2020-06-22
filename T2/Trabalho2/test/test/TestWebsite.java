ac224d984200bcf7c8d3333792d8b41c520bc074
Augusto Gai Dal'Asta <agasta@inf.ufsm.br>
2020-06-22 13:55:49 -0300

    Update TestWebsite.java

--- T2/Trabalho2/test/test/TestWebsite.java
+++ T2/Trabalho2/test/test/TestWebsite.java
@@ -12,8 +12,6 @@ import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 
-
-
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Ignore;
@@ -27,7 +25,8 @@ import org.openqa.selenium.support.ui.Select;
 public class TestWebsite {
     // constants
     public static String SITE_URL = "https://www.uni-hamburg.de/";
-    public static String PATH_DRIVE = "/home/flan/Documents/UFSM/5º Semestre/Qualidade/selenium-java-3.141.59/geckodriver";
+    public static String PATH_DRIVE = "C:\\Users\\augus\\Documents\\Eclipse\\qualidade\\assets\\Drivers\\geckodriver.exe";
+    //public static String PATH_DRIVE = "/home/flan/Documents/UFSM/5º Semestre/Qualidade/selenium-java-3.141.59/geckodriver";
     private static WebDriver driver; // //objeto que contem os metodos que controlam o navegador web
     WebElement element;
 
@@ -62,8 +61,8 @@ public class TestWebsite {
     }
     
     // testa se um video existe -> component 4: <video>
-    @Ignore
     @Test
+    @Ignore
     public void testVideo () throws InterruptedException {
         element = driver.findElement(By.tagName("video"));
         assertTrue(element != null);
@@ -76,29 +75,16 @@ public class TestWebsite {
         js.executeScript("document.getElementsByTagName(\"video\")[1].load();");
     }
     
-    // testa se o botão de scroll é exibido na página -> component 5: <button>
-    @Test
-    public void testButtonRollUp () throws InterruptedException {
-        JavascriptExecutor js = (JavascriptExecutor) driver;
-        js.executeScript("window.scrollBy(0, 8000);");
-        Thread.sleep(10000);
-        element = driver.findElement(By.id("scrollTopButton2"));
-        element.click();
-        Thread.sleep(2000);
-        int offset = (int) js.executeScript("return window.pageYOffset;");
-        System.out.println("OFFSEEEEEEEEEEEET: " + offset);
-        assertEquals(0, offset);
-    }
-    
     // Testa se o título da página é o esperado -> component 1: <title>
     @Test
+    @Ignore
     public void testPageTitle () {
         assertEquals("Universität Hamburg", driver.getTitle());
     }
 
     // testa todas as opções disponíveis em um comboBox -> component 2: <select>
-    @Ignore
     @Test
+    @Ignore
     public void testSelectButton () {
         // vai para a página de contato (em ingles)
         driver.navigate().to(SITE_URL + "en/newsroom/presse/kontakt.html");
@@ -120,8 +106,8 @@ public class TestWebsite {
     }
 
     // testa se a quantidade de elementos do select é o esperado
-    @Ignore
     @Test
+    @Ignore
     public void testSelectSize () {
         driver.navigate().to(SITE_URL + "en/newsroom/presse/kontakt.html");
         Select select = new Select(driver.findElement(By.id("institute")));
@@ -131,30 +117,30 @@ public class TestWebsite {
     }
 
     // testa se o botão submit da busca existe -> component 3: <input>
-    @Ignore
     @Test
+    @Ignore
     public void testSubmitSearch () {
         element = driver.findElement(By.cssSelector("input[type='submit']"));
         assertTrue(element != null);
     }
     
-    @Ignore
     @Test
+    @Ignore
     public void testField(){
         element = driver.findElement(By.name("q"));		 
         assertEquals("", element.getText());
     }
     
     // component 6: <a>
-    @Ignore
     @Test
+    @Ignore
     public void testLinkTwitter () {
         element = driver.findElement(By.className("twitter"));
         assertTrue(element.isDisplayed());
     }
     
-    @Ignore
     @Test
+    @Ignore
     public void testLinkTwitter2(){
         element = driver.findElement(By.className("twitter"));
         String link = element.getAttribute("href");
@@ -163,32 +149,82 @@ public class TestWebsite {
     }
     
     // testa os links da pagina
-    @Ignore
     @Test
+    @Ignore
     public void testAllLinks () {
         List<WebElement> list = new ArrayList();
+        boolean isValid = true;
+        
         list = driver.findElements(By.tagName("a"));
         for (WebElement e : list) {
             String link = e.getAttribute("href");
-            System.out.println(link);
-            boolean isValid = verificaConexao(link);
-            assertTrue(isValid);
+            System.out.print(link);
+            if (verificaConexao(link) == false) {
+            	isValid = false;
+            	System.out.print(" FAIL\n");
+            } else {
+            	System.out.print(" OK\n");
+            }
         }
+        
+        assertTrue(isValid);
     }
     
     // component 7: <p>
-    @Ignore
     @Test
+    @Ignore
     public void testCopyright() {
     	element = driver.findElement(By.className("copyright"));
     	assertEquals(element.getText(), "© 2020 Universität Hamburg. Alle Rechte vorbehalten");
     }
     
     // component 8: <footer>
-    @Ignore
     @Test
+    @Ignore
     public void testFooter () {
     	element = driver.findElement(By.id("fuss"));
     	assertTrue(element.isDisplayed());
+    }
+    
+ // testa se o botão de scroll é exibido na página -> component 5: <button>
+    @Test
+    @Ignore
+    public void testScrollToTopButton () throws InterruptedException {
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        js.executeScript("window.scrollTo(0, document.body.scrollHeight)");
+        Thread.sleep(10000);
+        
+        element = driver.findElement(By.id("scrollTopButton2"));
+        assertTrue(element.isDisplayed());
+    }
+    
+    @Test
+    @Ignore
+    public void testScrollToTopButtonAfterClick () throws InterruptedException {
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        js.executeScript("window.scrollTo(0, document.body.scrollHeight)");
+        Thread.sleep(10000);
+        
+        element = driver.findElement(By.id("scrollTopButton2"));
+        element.click();
+        Thread.sleep(5000);
+        
+        assertTrue(!(element.isDisplayed()));
+    }
+    
+    @Test
+    public void testButtonRollUp () throws InterruptedException {
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        js.executeScript("window.scrollTo(0, document.body.scrollHeight)");
+        
+        Thread.sleep(10000);
+        element = driver.findElement(By.id("scrollTopButton2"));
+        element.click();
+        
+        Thread.sleep(5000);
+        long offset = (long) js.executeScript("return window.scrollY");
+        System.out.println("Offset: " + offset);
+        
+        assertEquals((long) 0, offset);
     }
 }
